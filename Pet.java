65a51973ea4171cc11787166e1ab92d0a1c03bd9
consumerofbeanss <ostein.vellim@binus.ac.id>
2023-05-31 10:19:37 +0700

    Created Pet Class

---
+++ Pet.java
@@ -0,0 +1,10 @@
+public class Pet {
+    private String name;
+    private int age;
+    private int hunger;
+    private int health;
+    private int fun;
+    private String energy;
+    private int time;
+
+}
