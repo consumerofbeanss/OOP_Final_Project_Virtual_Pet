8b2c81d6a51e4eea409d2738fa54f7f17abead54
consumerofbeanss <ostein.vellim@binus.ac.id>
2023-06-03 23:06:14 +0700

    Added Main

--- Pet.java
+++ Pet.java
@@ -1,21 +1,24 @@
+import javax.xml.namespace.QName;
+
 public class Pet {
     protected String name;
-    protected int age;
     protected int saturation;
-    protected int health;
     protected int fun;
     protected int energy;
-    protected int time;
     protected int money;
 
     public Pet(){
-        this.name = "Steve";
         this.saturation = 100;
-        this.energy = 100;
+        this.energy = 200;
+        this.fun = 100;
         this. money = 5000;
     }
 
-
-
-
+    public Pet(String name, int saturation, int fun, int energy, int money) {
+        this.name = name;
+        this.saturation = saturation;
+        this.fun = fun;
+        this.energy = energy;
+        this.money = money;
+    }
 }
